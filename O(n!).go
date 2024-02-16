439431ad6b166c9650e6ead2cf181cebf216f43f
Daniil Stepanov <132468035+c0mrade12211@users.noreply.github.com>
2024-02-16 21:38:20 +0300

    Create O(n!).go

---
+++ O(n!).go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n <= 1 {
+		return 1
+	}
+	fmt.Println(n)
+	return n * factorial(n-1)
+}
+
+func main() {
+	number := 4
+	result := factorial(number)
+	fmt.Printf("Факториал числа %d равен %d\n", number, result)
+}
